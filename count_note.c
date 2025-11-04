374a3d730a9e1ecec7ff1675c04e380ef6cf4645
sujaldhumal20 <sujaldhumal20@gmail.com>
2025-11-04 22:36:08 +0530

    Add files via upload

---
+++ count_note.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+void main(){
+    int num,note;
+    printf("Enter a amount : ");
+    scanf("%d",&num);
+    note=num/50;
+    printf("The given amount has %d no. of 50rs note",note);
+}
